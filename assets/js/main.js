c72eec539bc353edd408243e5908e8f53c3b48b9
abdulazizch <abdulaziz.dev2@gmail.com>
2023-03-01 13:57:56 +0500

    Menu Items Loaded from json, video moved to background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,177 +1,242 @@
-var j = -1;
-$(document).ready(function(){
-    var menuItems = $(".navbar-menu .navbar-item")
-    var subMenuItems = $(".navbar-item.active .nav-sub-item")
-    var i = 0;
-    $(".site-logo").mouseenter(function(){
-        $(".navbar-item.item-1").addClass("hovered")
-    })
-    $(".navbar-menu .navbar-item").mouseenter(function(){
-        $(".navbar-menu .navbar-item").removeClass("active")
-        $(".navbar-menu .navbar-item").removeClass("hovered")
-        $(".navbar-item .nav-sub-item").removeClass("active")
-        $(".search-input-label").html("Search . "+$(this).find("a").text())
-        $(".searchbar-input-con").removeClass("active")
-        $(".searchbar-input-con").parent().removeClass("active")
-        $(this).addClass("hovered")
-        $(this).addClass("active")
-        $(this).find("a").click(function(e){
-            e.preventDefault();
+// Script to Load Menu items 
+// Menu and version data fetching function
+$(function(){
+    if(document.querySelector(".navbar")){
+        //Menu JSON File URL
+        const dataApi_url = "./assets/js/menu_structure.json";
+        var data = '';
+
+        //async function to get Data from JSON file
+        async function getMenuData(url) {
+            // Storing response
+            const response = await fetch(url);
+
+            // Storing data in form of JSON
+            data = await response.json();
+            var $menu = $("#navbar_menu_list");
+            $.each(data.menu, function (index) {
+                $menu.append(
+                    getMenuItem(this, index)
+                );
+            });
+            applyMenuScript()
+
+        } 
+        // Calling the async function
+        getMenuData(dataApi_url);
+       
+
+        var getMenuItem = function (itemData, index) {
+        
+            var item = $('<li class="navbar-item">')
+                item.append( $('<a href="" class="navbar-link">').html(itemData.name));
+            if (itemData.sub) {
+                var subListCon = $('<div class="nav-sub-menu">');
+                var subList = $('<ul class="sub-menu-list">');
+                $.each(itemData.sub, function (subIndex) {
+                    subList.append(getSubMenuItem(this, subIndex));
+                });
+                subListCon.append(subList);
+                item.append(subListCon);
+                $(item).addClass("item-"+(index+1))
+                return item;
+            }
+            
+        };
+        var getSubMenuItem = function (itemData, index) {
+            var item = ''
+                item = $('<li class="nav-sub-item">')
+                $(item).html(itemData.name);
+                $(item).addClass("item-"+(index+1))
+            return item;
+
+        };
+        
+    }
+})
+
+
+function applyMenuScript(){
+    var j = -1;
+    $(document).ready(function(){
+        var menuItems = $(".navbar-menu .navbar-item")
+        var subMenuItems = $(".navbar-item.active .nav-sub-item")
+        var i = 0;
+        $(".site-logo").mouseenter(function(){
+            $(".navbar-item.item-1").addClass("hovered")
         })
-        subMenuItems = $(".navbar-item.active .nav-sub-item")
-        activeSearchbar()
-    })
-    $(".navbar").mouseleave(function(){
-        $(".navbar-menu .navbar-item").removeClass("hovered")
-        if(!document.querySelector(".navbar-item .nav-sub-item.active")){
+        $(".navbar-menu .navbar-item").mouseenter(function(){
             $(".navbar-menu .navbar-item").removeClass("active")
-            $(".search-input-label").html("Search . ")
+            $(".navbar-menu .navbar-item").removeClass("hovered")
+            $(".navbar-item .nav-sub-item").removeClass("active")
+            $(".search-input-label").html("Search . "+$(this).find("a").text())
             $(".searchbar-input-con").removeClass("active")
             $(".searchbar-input-con").parent().removeClass("active")
-        }
-        subMenuItems = $(".navbar-item.active .nav-sub-item")
-    })
-    $(".navbar-control.next").click(function(){
-        nextItem()
-    })
-    $(".navbar-control.prev").click(function(){
-        prevItem()
-    })
-   
-    $(".navbar-control.up").click(function(){
-        upItem();
-    })
-    $(".navbar-control.down").click(function(){
-        downItem();
-    })
-    $(".navbar-item .nav-sub-item").click(function(){
-        subMenuItems = $(".navbar-item.active .nav-sub-item")
-        $(".navbar-item .nav-sub-item").removeClass("active")
-        $(this).addClass("active")
-        activeSearchbar()
-        $(".progress-title-con .progress-title").html($(this).text())
-    })
-    $(".navbar-item .nav-sub-item").mouseenter(function(){
-        $(this).addClass("active")
-        activeSearchbar()
-        $(".navbar-item .nav-sub-item").removeClass("active")
-    })
-
-    document.onkeyup = function(event) {
-            switch (event.keyCode) {
-               case 37:
-                    prevItem()
-                    activeSearchbar()
-                  break;
-               case 38:
-                    upItem()
-                    activeSearchbar()
-                  break;
-               case 39:
-                    nextItem()
-                    activeSearchbar()
-                  break;
-               case 40:
-                    downItem()
-                    activeSearchbar()
-            }        
-    };
-    
-    function activeSearchbar(){
-        $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(".navbar-item .nav-sub-item.active").text())
-        $(subMenuItems).each(function(index) {
-            if(this.classList.contains("active")){
-                j=index
-            }
-          });
-          $(".searchbar-input-con").addClass("active")
-          $(".searchbar-input-con").parent().addClass("active")
-    }
-    function nextItem(){
-        if(j <=-1){
-            $(".navbar-menu .navbar-item").removeClass("active")
-            if(i < menuItems.length){
-                $(menuItems[i]).addClass("active")
-                i++
-            }
-            else{
-                i = 0;
-                $(menuItems[i]).addClass("active")
-                i++
-            }
-        }
-        else if(j > -1 && j < subMenuItems.length){
-            j++
-            $(".navbar-item .nav-sub-item").removeClass("active")
-            $(subMenuItems[j]).addClass("active")
-            $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
-            $(".progress-title-con .progress-title").html($(this).text())
-            $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
-        }
-        else{
-            j=0;
-            $(subMenuItems[j]).addClass("active")
-            $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
-            $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
-        }
-    }
-    function prevItem(){
-        if(j <= -1){
-            $(".navbar-menu .navbar-item").removeClass("active")
-            if(i >= 0){
-                i--
-                $(menuItems[i]).addClass("active")
-            }
-            else{
-                i = menuItems.length - 1;
-                $(menuItems[i]).addClass("active")
-            }
-        }
-        else if(j > -1){
-            j--
-            if(j == -1){
-                j=0
-            }
-            $(".navbar-item .nav-sub-item").removeClass("active")
-            $(subMenuItems[j]).addClass("active")
-            $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
-            $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
-        }
-        else{
-            j= subMenuItems.length - 1;
-        }
-    }
-
-    function downItem(){
-        if(document.querySelector(".navbar-item.active")){
+            $(this).addClass("hovered")
+            $(this).addClass("active")
+            $(this).find("a").click(function(e){
+                e.preventDefault();
+            })
             subMenuItems = $(".navbar-item.active .nav-sub-item")
-            if(j == -1){
+            activeSearchbar()
+        })
+        $(".navbar").mouseleave(function(){
+            $(".navbar-menu .navbar-item").removeClass("hovered")
+            if(!document.querySelector(".navbar-item .nav-sub-item.active")){
+                $(".navbar-menu .navbar-item").removeClass("active")
+                $(".search-input-label").html("Search . ")
+                $(".searchbar-input-con").removeClass("active")
+                $(".searchbar-input-con").parent().removeClass("active")
+            }
+            subMenuItems = $(".navbar-item.active .nav-sub-item")
+        })
+        $(".navbar-control.next").click(function(){
+            nextItem()
+        })
+        $(".navbar-control.prev").click(function(){
+            prevItem()
+        })
+       
+        $(".navbar-control.up").click(function(){
+            upItem();
+        })
+        $(".navbar-control.down").click(function(){
+            downItem();
+        })
+        $(".navbar-item .nav-sub-item").click(function(){
+            subMenuItems = $(".navbar-item.active .nav-sub-item")
+            $(".navbar-item .nav-sub-item").removeClass("active")
+            $(this).addClass("active")
+            activeSearchbar()
+            $(".progress-title-con .progress-title").html($(this).text())
+        })
+        $(".navbar-item .nav-sub-item").mouseenter(function(){
+            $(this).addClass("active")
+            activeSearchbar()
+            $(".navbar-item .nav-sub-item").removeClass("active")
+        })
+    
+        document.onkeyup = function(event) {
+                switch (event.keyCode) {
+                   case 37:
+                        prevItem()
+                        activeSearchbar()
+                      break;
+                   case 38:
+                        upItem()
+                        activeSearchbar()
+                      break;
+                   case 39:
+                        nextItem()
+                        activeSearchbar()
+                      break;
+                   case 40:
+                        downItem()
+                        activeSearchbar()
+                }        
+        };
+        
+        function activeSearchbar(){
+            $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(".navbar-item .nav-sub-item.active").text())
+            $(subMenuItems).each(function(index) {
+                if(this.classList.contains("active")){
+                    j=index
+                }
+              });
+              $(".searchbar-input-con").addClass("active")
+              $(".searchbar-input-con").parent().addClass("active")
+        }
+        function nextItem(){
+            if(j <=-1){
+                $(".navbar-menu .navbar-item").removeClass("active")
+                $(".navbar-menu .navbar-item").removeClass("hovered")
+                if(i < menuItems.length){
+                    $(menuItems[i]).addClass("active")
+                    i++
+                }
+                else{
+                    i = 0;
+                    $(menuItems[i]).addClass("active")
+                    i++
+                }
+            }
+            else if(j > -1 && j < subMenuItems.length){
                 j++
+                $(".navbar-item .nav-sub-item").removeClass("active")
+                $(".navbar-item .nav-sub-item").removeClass("hovered")
+                $(subMenuItems[j]).addClass("active")
+                $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
+                $(".progress-title-con .progress-title").html($(this).text())
+                $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
+            }
+            else{
+                j=0;
                 $(subMenuItems[j]).addClass("active")
                 $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
                 $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
-                $(".searchbar-input-con").addClass("active")
-                $(".searchbar-input-con").parent().addClass("active")
             }
         }
-    }
-    function upItem(){
-        if(document.querySelector(".navbar-item.active")){
-            $(".nav-sub-item").removeClass("active")
-            j = -1;
-            $(".searchbar-input-con").removeClass("active")
-            $(".searchbar-input-con").parent().removeClass("active")
-            $(".search-input-label").html("Search .")
-            $(".progress-title-con .progress-title").html("")
+        function prevItem(){
+            if(j <= -1){
+                $(".navbar-menu .navbar-item").removeClass("active")
+                $(".navbar-menu .navbar-item").removeClass("hovered")
+                if(i >= 0){
+                    i--
+                    $(menuItems[i]).addClass("active")
+                }
+                else{
+                    i = menuItems.length - 1;
+                    $(menuItems[i]).addClass("active")
+                }
+            }
+            else if(j > -1){
+                j--
+                if(j == -1){
+                    j=0
+                }
+                $(".navbar-item .nav-sub-item").removeClass("active")
+                $(".navbar-item .nav-sub-item").removeClass("hovered")
+                $(subMenuItems[j]).addClass("active")
+                $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
+                $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
+            }
+            else{
+                j= subMenuItems.length - 1;
+            }
         }
-    }
-})
+    
+        function downItem(){
+            if(document.querySelector(".navbar-item.active")){
+                subMenuItems = $(".navbar-item.active .nav-sub-item")
+                if(j == -1){
+                    j++
+                    $(subMenuItems[j]).addClass("active")
+                    $(".search-input-label").html("Search . "+$(".navbar-item.active a").text()+" ."+$(subMenuItems[j]).text())
+                    $(".progress-title-con .progress-title").html($(subMenuItems[j]).text())
+                    $(".searchbar-input-con").addClass("active")
+                    $(".searchbar-input-con").parent().addClass("active")
+                }
+            }
+        }
+        function upItem(){
+            if(document.querySelector(".navbar-item.active")){
+                $(".nav-sub-item").removeClass("active")
+                j = -1;
+                $(".searchbar-input-con").removeClass("active")
+                $(".searchbar-input-con").parent().removeClass("active")
+                $(".search-input-label").html("Search .")
+                $(".progress-title-con .progress-title").html("")
+            }
+        }
+    })
+}
+
 
 // Video Controls Styles 
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
 const progressTitleCon = document.querySelector('.progress-title-con');
-const player = document.querySelector(".video-player-container");
+const player = document.querySelector(".bg_video_section");
 const video = player.querySelector('.viewer');
 
 let muted = false;
@@ -234,18 +299,15 @@ function scrub(event) {
 function togglePlay() {
     if (video.paused) {
       video.play();
-    //   playBtn.classList.replace('fa-play', 'fa-pause');
-    //   playBtn.setAttribute('title', 'Pause');
     } else {
       video.pause();
-    //   showPlayIcon();
     }
 }
 
 // Spacebar used to play and pause
 document.body.onkeyup = function (e) {
     if (e.keyCode == 32) {
-      togglePlay();
+    //   togglePlay();
     }
     if(e.keyCode == 13){
         const elem = document.activeElement;
@@ -367,10 +429,10 @@ $(document).ready(function(){
             togglePlay();
         }, 2000);
     }
-    $(".video-player-container").mouseenter(function(){
-        video.play()
-    })
-    $(".video-player-container").mouseleave(function(){
-        video.pause()
-    })
+    // $(".video-player-container").mouseenter(function(){
+    //     video.play()
+    // })
+    // $(".video-player-container").mouseleave(function(){
+    //     video.pause()
+    // })
 })
